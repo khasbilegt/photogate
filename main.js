0954f95d0471d3d18485f1cda7f65eb1efe9465a
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2018-05-24 17:30:01 +0800

    add splash window

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+// Inter-Process Communication (IPC)
+const ipcMain = electron.ipcMain
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -10,10 +12,18 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let splashWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    show: false,
+    backgroundColor: '#fff',
+    width: 800, 
+    height: 600,
+    minHeight: 350,
+    minWidth: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -34,10 +44,37 @@ function createWindow () {
   })
 }
 
+function createSplashWindow () {
+  splashWindow = new BrowserWindow({
+    width: 320,
+    height: 240,
+    frame: false,
+    resizable: false,
+    backgroundColor: '#ccc',
+    alwaysOnTop: true,
+    show: false,
+  })
+
+  splashWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'splash.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+
+  splashWindow.on('closed', () => {
+    splashWindow = null
+  })
+
+  splashWindow.once('ready-to-show', () => {
+    splashWindow.show()
+    createWindow()
+  })
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createSplashWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -58,3 +95,17 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on('get-version', event => {
+  console.log('app-version: ', app.getVersion())
+  event.sender.send('set-version', app.getVersion())
+})
+
+  ipcMain.on('app-init', event => {
+    if (splashWindow) {
+      setTimeout(() => {
+        splashWindow.close()
+      }, 2000)
+    }
+
+    mainWindow.show()
+  })
