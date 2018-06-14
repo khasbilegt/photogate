312dae14364646f23110e177ffc8760f9e95a1c5
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2018-06-14 15:20:47 +0800

    add notification, alert feature, display and table change buttons, stop button

--- photogate/global/js/Custom.js
+++ photogate/global/js/Custom.js
@@ -2,9 +2,3 @@
 $('#objectLength-gate').tooltip({'trigger':'hover', 'title':'Хэмжих гэж буй биетийн нийт урт', 'placement':'bottom'});
 $('#objectLength-interval').tooltip({'trigger':'hover', 'title':'Хэмжих гэж буй биетийн нийт урт', 'placement':'right'});
 $('#distanceLength').tooltip({'trigger':'hover', 'title':'Хэмжигчүүдийн хоорондын зай', 'placement':'bottom'})
-
-// $(document).ready( function () {
-//     $('#table').DataTable({
-//         data: arrayData,
-//     })
-// })
--- renderer.js
+++ renderer.js
@@ -3,8 +3,6 @@
 // All of the Node.js APIs are available in this process.
 const { ipcRenderer } = require('electron')
 const serialport = require('serialport')
-const SerialPort = serialport.SerialPort
-const teensy_id = "4304420"
 
 window.$ = window.jQuery = require('./photogate/global/vendor/jquery/jquery.min.js');
 
@@ -17,6 +15,76 @@ setTimeout(() => {
 
 $('#run-gate').click(() => {
     let objectLength = $('#objectLength-gate').val();
+    let isGraph = ($("#graph-gate").prop("checked")) 
+    let isTable = ($('#table-gate').prop("checked"))
+    let table = $('#table').DataTable()
+    table.clear().draw()
+
+    serialport.list((err, ports) => {
+        if (err)
+            errorHandler(err.message, 'error', 'Алдаа')
+        
+        ports.forEach(port => {
+            if (port.manufacturer === "Teensyduino") {
+                if (isGraph) {
+                    $('#graphContainer').show()
+                } 
+                else {
+                    $('#tableContainer').show()
+                    $('#tableHeader-gate').show()
+                }
+
+                $('#display-section').show()
+                $('#presentation-section').hide()
+
+                photogate = new serialport(port.comName, {
+                    baudRate: 115200,
+                    parser: new serialport.parsers.Readline('\r\n'),
+                })
+                
+                if (!photogate.isOpen) {
+                    photogate.write('l' + objectLength, (err) => {
+                        if (err) {
+                            errorHandler(err.message, 'warning','Анхаар')
+                        }
+                        console.log('Set object length')
+
+                        photogate.drain(() => {
+                            photogate.flush()
+                        })
+                    })
+                    
+                    photogate.write('g1', (err) => {
+                        if (err) {
+                            errorHandler(err.message, 'error','Алдаа')
+                        }
+                        console.log('Started gate mode')
+                    })
+                
+                    photogate.on('readable', () => {
+                        let data = photogate.read().toString('utf8').slice()
+                        console.log(data)
+                        arrayData = data.split(" ");
+                        if (arrayData.length == 5) {
+                            table.row.add(arrayData).draw()
+                        }
+                        console.log(arrayData)
+                    })
+
+                    photogate.on('close', () => {
+                        console.log("Port closed")
+                        errorHandler("Хэмжилтийг зогсоолоо", "warning", "Анхаар")
+                    })
+                }
+            }
+        });
+    })
+})
+
+$('#run-interval').click(() => {
+    let objectLength = $('#objectLength-interval').val();
+    let distanceLength = $('#distanceLength').val();
+    
     let display = ($("#graph-gate").prop("checked", true)) ? true : false
 
     let table = $('#table').DataTable()
@@ -33,11 +101,13 @@ $('#run-gate').click(() => {
                 
                 
                 if (!photogate.isOpen) {
+                    photogate.write('l' + objectLength)
+                    photogate.write('d' + distanceLength)
+
                     photogate.write('g1')
                 
                     photogate.on('readable', () => {
                         let data = photogate.read().toString('utf8').slice()
-                        console.log(data)
                         arrayData = data.split(" ");
                         table.row.add(arrayData).draw()
                         console.log(arrayData)
@@ -52,12 +122,62 @@ $('#run-gate').click(() => {
     })
 })
 
+$('#stop-gate').click(() => {
+    photogate.write('q')
+    photogate.drain(() => {
+        photogate.flush(() => {
+            photogate.close()
+        })
+    })
 
-function errorHandler(err) {
-    $('#error').text(err)
-}
-
-function sendCommand(cmd) {
-    console.log('Command ' + cmd)
+    // photogate.write('r')
+    // photogate.drain(() => {
+    //     photogate.on('data', (data) => {
+    //         console.log(data)
+    //     })
+    // })
     
+    // photogate.close()
+})
+$('#stop-interval').click(() => {
+    photogate.write('q')
+    photogate.close()
+})
+
+$('#showTable').click(() => {
+    if ($('#tableContainer:hidden')) {
+        $('#tableContainer').show()    
+        $('#graphContainer').hide()
+    }
+})
+
+$('#showGraph').click(() => {
+    if ($('#graphContainer:hidden')) {
+        $('#graphContainer').show()
+        $('#tableContainer').hide()
+    }
+})
+
+function errorHandler(err, type, title) {
+    if (err && type && title) {
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-top-full-width",
+            "preventDuplicates": true,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        
+        toastr[type](err, title)
+    }
 }
