a31ad827987e4ad2241318fb2fc776ea6b6d7154
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2018-07-25 13:27:00 +0800

    change tooltip placement

--- photogate/global/js/Custom.js
+++ photogate/global/js/Custom.js
@@ -1,4 +1,4 @@
 // Custom JS codes
 $('#objectLength-gate').tooltip({'trigger':'hover', 'title':'Хэмжих гэж буй биетийн нийт урт', 'placement':'bottom'});
-$('#objectLength-interval').tooltip({'trigger':'hover', 'title':'Хэмжих гэж буй биетийн нийт урт', 'placement':'right'});
+$('#objectLength-interval').tooltip({'trigger':'hover', 'title':'Хэмжих гэж буй биетийн нийт урт', 'placement':'bottom'});
 $('#distanceLength').tooltip({'trigger':'hover', 'title':'Хэмжигчүүдийн хоорондын зай', 'placement':'bottom'})
